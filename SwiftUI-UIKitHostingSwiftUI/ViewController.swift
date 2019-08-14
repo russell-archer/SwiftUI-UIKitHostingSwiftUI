621dacdec645bc52595ed917c4e265df07fcf620
russell-archer <russell.archer@mac.com>
2019-08-14 22:56:20 +0100

    Initial Commit

---
+++ SwiftUI-UIKitHostingSwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUI-UIKitHostingSwiftUI
+//
+//  Created by Russell Archer on 14/08/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
